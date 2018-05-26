0b711a6bdc3834ddacf5e177190f0478d938fae7
IT Admin <admin@itadmin.com.mx>
2018-05-26 12:18:56 -0500

    [FIX] CFDI fill automation, fix complemento de pago, factura global

--- custom_invoice/__manifest__.py
+++ custom_invoice/__manifest__.py
@@ -1,31 +1,34 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#                 @author Esousy
+#                 @author IT Admin
 #
 ##############################################################################
 
 {
     'name': 'Punto de Venta Factura Electronica Mexico CFDI',
-    'version': '9.0',
+    'version': '2.0',
     'description': ''' Punto de Venta Factura Electronica Mexico (CFDI 3.3).
     ''',
-    'category': 'Sales, Point Of Sale, Accounting',
+    'category': 'Accounting',
     'author': 'IT Admin',
     'website': '',
     'depends': [
         'base',
         'point_of_sale',
-        'sale','account'
+        'sale','account','account_accountant', 'purchase'
     ],
     'data': [
         'views/res_partner_view.xml',
         'views/res_company_view.xml',
         'views/product_view.xml',
         'views/account_invoice_view.xml',
+        'views/invoice_supplier_view.xml',
         'views/account_payment_view.xml',
         'views/account_tax_view.xml',
         'views/point_of_sale_view.xml',
         'views/sale_view.xml',
+        'views/account_payment_term_view.xml',
+        'views/purchase_view.xml',
         'wizard/create_invoice_wizard.xml',
         'wizard/create_invoice_total_wizard.xml',
         'wizard/create_invoice_session_wizard.xml',
