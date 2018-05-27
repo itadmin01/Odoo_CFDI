b8762b69ef8608c9d25cbb8e97c072d36c368a4b
IT Admin <admin@itadmin.com.mx>
2018-05-27 10:50:36 -0500

    [FIX] CFDI fill automation, fix complemento de pago, factura global

--- cdfi_invoice/__openerp__.py
+++ cdfi_invoice/__openerp__.py
@@ -1,29 +1,32 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#                 @author Esousy
+#                 @author IT Admin
 #
 ##############################################################################
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '9.0',
-    'description': ''' Factura Electronica módulo de ventas para Mexico (CFDI 2017)
+    'version': '2.0',
+    'description': ''' Factura Electronica módulo de ventas para Mexico (CFDI 3.3)
     ''',
-    'category': 'Sales, Accounting',
+    'category': 'Accounting',
     'author': 'IT Admin',
     'website': '',
     'depends': [
         'base',
-        'sale','account'
+        'sale','account','account_accountant', 'purchase'
     ],
     'data': [
         'views/res_partner_view.xml',
         'views/res_company_view.xml',
         'views/product_view.xml',
         'views/account_invoice_view.xml',
+        'views/invoice_supplier_view.xml',
         'views/account_payment_view.xml',
         'views/account_tax_view.xml',
         'views/sale_view.xml',
+        'views/account_payment_term_view.xml',
+        'views/purchase_view.xml',
         'report/invoice_report.xml',
         'report/payment_report.xml',
         'report/sale_report_templates.xml',
