e9631fbd223f6d3935b7f097fdbc73ec4d9f7ec7
IT Admin <admin@itadmin.com.mx>
2018-11-24 13:44:12 -0500

    [FIX] CFDI cancel update

--- cdfi_invoice/__openerp__.py
+++ cdfi_invoice/__openerp__.py
@@ -6,14 +6,14 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '2.0',
+    'version': '8.2',
     'description': ''' Factura Electronica módulo de ventas para Mexico (CFDI 3.3)
     ''',
     'category': 'Accounting',
     'author': 'IT Admin',
     'website': '',
     'depends': ['sale', 'purchase','account', 'procurement'
-    ],#'base',,'account', 'account_voucher', 'procurement'
+    ],
     'data': [
         'views/res_partner_view.xml',
         'views/res_company_view.xml',
@@ -25,6 +25,8 @@
         'views/sale_view.xml',
         'views/account_payment_term_view.xml',
         'views/purchase_view.xml',
+        'views/account_journal_view.xml',
+        'wizard/import_account_payment_view.xml',
         'report/invoice_report.xml',
         'report/payment_report.xml',
         'report/sale_report_templates.xml',
