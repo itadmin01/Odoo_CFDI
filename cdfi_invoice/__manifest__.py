069a5aef48afec0d0e7d76c57c67fbc2642c7db5
IT Admin <admin@itadmin.com.mx>
2019-03-24 08:42:08 -0500

    [FIX] Cancelación rechazada update

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,12 +6,12 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '12.3',
+    'version': '12.4',
     'description': ''' Factura Electronica módulo de ventas para Mexico (CFDI 3.3)
     ''',
     'category': 'Accounting',
     'author': 'IT Admin',
-    'website': '',
+    'website': 'www.itadmin.com.mx',
     'depends': [
         'base',
         'sale','account','purchase'
