e75d96c4adc666a45d6c8f8f9b6e1658ef2720d5
IT Admin <admin@itadmin.com.mx>
2020-02-26 18:58:10 -0500

    [FIX] Update version

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '13.1',
+    'version': '13.2',
     'description': ''' Factura Electronica módulo de ventas para Mexico (CFDI 3.3)
     ''',
     'category': 'Accounting',
