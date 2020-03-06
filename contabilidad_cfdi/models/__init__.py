1486e701d9efb974fbbd3a368d1d7ecd05d3a19c
IT Admin <admin@itadmin.com.mx>
2020-03-06 18:15:59 -0500

    [FIX] Remove module

--- contabilidad_cfdi/models/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import account_account
-from . import res_currency
-from . import account_group
-from . import account_move_line
-#from . import account_invoice
-from . import account_move
-from . import res_partner
