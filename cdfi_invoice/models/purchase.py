a0cf33fd88daaa5a9b7069644a512a632bc23a70
IT Admin <admin@itadmin.com.mx>
2020-04-02 17:13:33 -0500

    [FIX] Update Forma de pago in missing models

--- cdfi_invoice/models/purchase.py
+++ cdfi_invoice/models/purchase.py
@@ -32,7 +32,8 @@ class PurchaseOrder(models.Model):
                    ('27', '27 - A satisfacción del acreedor'), 
                    ('28', '28 - Tarjeta de débito'), 
                    ('29', '29 - Tarjeta de servicios'), 
-                   ('30', '30 - Aplicación de anticipos'), 
+                   ('30', '30 - Aplicación de anticipos'),
+                   ('31', '31 - Intermediario pagos'), 
                    ('99', '99 - Por definir'),],
         string=_('Forma de pago'),
     )
