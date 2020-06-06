8e16c7566df7723fcda5975dbdb8a3031f5797b2
IT Admin <admin@itadmin.com.mx>
2020-06-06 12:44:33 -0500

    [FIX] Add taxes to PDF report

--- cdfi_invoice/models/account_invoice.py
+++ cdfi_invoice/models/account_invoice.py
@@ -26,7 +26,8 @@ class AccountMove(models.Model):
     tipo_comprobante = fields.Selection(
         selection=[('I', 'Ingreso'), 
                    ('E', 'Egreso'),
-                    ('T', 'Traslado'),],
+                  #('T', 'Traslado'),
+                  ],
         string=_('Tipo de comprobante'),
     )
     forma_pago = fields.Selection(
