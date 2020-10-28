18a944dc2e30c2d0eabc5f4122bd7bda1e3f82ce
IT Admin <admin@itadmin.com.mx>
2020-10-28 05:35:16 -0500

    [FIX] Manifest

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -37,7 +37,7 @@
         'data/mail_template_data.xml',
         'data/cron.xml',
     ],
-    'images': ['static/description/banner.png'],
+    'images': ['static/description/banner.jpg'],
     'application': False,
     'installable': True,
     'price': 0.00,
