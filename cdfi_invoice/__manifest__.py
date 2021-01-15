4471a98995793f3dfd3bb3d5a795a9303e67ac51
IT Admin <admin@itadmin.com.mx>
2021-01-15 08:39:55 -0500

    [FIX] hide cfdi button

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '13.8',
+    'version': '13.9',
     'description': ''' Factura Electronica módulo de ventas para Mexico (CFDI 3.3)
     ''',
     'category': 'Accounting',
