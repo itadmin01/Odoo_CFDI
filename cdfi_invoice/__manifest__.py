ccddf35daf45c632329a92770390e6c0194e9798
IT Admin <admin@itadmin.com.mx>
2021-04-03 09:58:45 -0500

    [FIX] Manage prices with included taxes

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '14.4',
+    'version': '14.5',
     'description': ''' Factura Electronica módulo de ventas para Mexico (CFDI 3.3)
     ''',
     'category': 'Accounting',
