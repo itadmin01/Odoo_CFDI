3b598c212c492f440c09cb1bee32de40c84f4fcd
Fernando Rivero <frivero@itadmin.com.mx>
2021-08-03 08:40:01 -0500

    [FIX] Add option to regimen fiscal

--- cdfi_invoice/models/res_company.py
+++ cdfi_invoice/models/res_company.py
@@ -43,7 +43,8 @@ class ResCompany(models.Model):
                    ('607', _('Régimen de Enajenación o Adquisición de Bienes')),
                    ('629', _('De los Regímenes Fiscales Preferentes y de las Empresas Multinacionales')),
                    ('630', _('Enajenación de acciones en bolsa de valores')),
-                   ('615', _('Régimen de los ingresos por obtención de premios')),],
+                   ('615', _('Régimen de los ingresos por obtención de premios')),
+                   ('625', _('Régimen de las Actividades Empresariales con ingresos a través de Plataformas Tecnológicas')),],
         string=_('Régimen Fiscal'), 
     )
     archivo_cer = fields.Binary(string=_('Archivo .cer'))
