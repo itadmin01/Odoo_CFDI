6d3e609bd4cfdd64ea71e60c6630511a9b527a19
Fernando Rivero <frivero@itadmin.com.mx>
2021-10-14 18:15:23 -0500

    [ADD] Module migration

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '14.7',
+    'version': '15.1',
     'description': ''' Factura Electronica módulo de ventas para Mexico (CFDI 3.3)
     ''',
     'category': 'Accounting',
