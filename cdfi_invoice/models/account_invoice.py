6374ab4b9eb2bc2082ef6a3ee75b4174c2489410
Fernando Rivero <frivero@itadmin.com.mx>
2022-05-11 16:11:16 -0500

    [FIX] Check partner name

--- cdfi_invoice/models/account_invoice.py
+++ cdfi_invoice/models/account_invoice.py
@@ -250,6 +250,8 @@ class AccountMove(models.Model):
     
     @api.model
     def to_json(self):
+        self.check_cfdi_values()
+
         if self.partner_id.vat == 'XAXX010101000' or self.partner_id.vat == 'XEXX010101000':
             zipreceptor = self.journal_id.codigo_postal or self.company_id.zip
             if self.factura_global:
@@ -284,8 +286,6 @@ class AccountMove(models.Model):
            tipocambio = 1
         else:
            tipocambio = self.set_decimals(1 / self.currency_id.with_context(date=self.invoice_date).rate, no_decimales_tc)
-
-        self.check_cfdi_values()
 
         request_params = {
                 'factura': {
@@ -600,6 +600,10 @@ class AccountMove(models.Model):
             self.write({'proceso_timbrado': False})
             self.env.cr.commit()
             raise UserError(_('El receptor no tiene RFC configurado.'))
+        if not self.partner_id.name:
+            self.write({'proceso_timbrado': False})
+            self.env.cr.commit()
+            raise UserError(_('El receptor no tiene nombre configurado.'))
         if not self.uso_cfdi:
             self.write({'proceso_timbrado': False})
             self.env.cr.commit()
