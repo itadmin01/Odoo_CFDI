15fa5e24a5dde9989a146057c0f9fc7b4c655828
Fernando Rivero <frivero@itadmin.com.mx>
2022-05-20 05:58:31 -0500

    [FIX] change to traditional round

--- cdfi_invoice/models/account_payment.py
+++ cdfi_invoice/models/account_payment.py
@@ -418,7 +418,7 @@ class AccountPayment(models.Model):
                   if line['ImpuestoP'] == '002':
                        totales.update({'TotalRetencionesIVA': self.set_decimals(line['ImporteP'],2),})
                   if line['ImpuestoP'] == '001':
-                       totales.update({'TotalRetencionesISR': self.set_decimals(line['ImporteP'],2),})
+                       totales.update({'TotalRetencionesISR': self.roundTraditional(line['ImporteP'],2),})
                   if line['ImpuestoP'] == '003':
                        totales.update({'TotalRetencionesIEPS': self.set_decimals(line['ImporteP'],2),})
                   self.total_pago -= round(line['ImporteP'] * float(self.tipocambiop),2)
@@ -521,6 +521,9 @@ class AccountPayment(models.Model):
         if amount is None or amount is False:
             return None
         return '%.*f' % (precision, amount)
+
+    def roundTraditional(self, val,digits):
+       return round(val+10**(-len(str(val))-1), digits)
 
     def clean_text(self, text):
         clean_text = text.replace('\n', ' ').replace('\\', ' ').replace('-', ' ').replace('/', ' ').replace('|', ' ')
