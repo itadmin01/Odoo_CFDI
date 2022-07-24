630646eee5c5ff1b8aa5b7744cb343ef9633e8ed
Fernando Rivero <frivero@itadmin.com.mx>
2022-07-24 11:13:55 -0500

    [FIX] payment values

--- cdfi_invoice/models/account_invoice.py
+++ cdfi_invoice/models/account_invoice.py
@@ -1002,65 +1002,6 @@ class AccountInvoice(models.Model):
                'target': 'new'
            }
 
-    @api.model
-    def _get_payments_vals(self):
-        if not self.payment_move_line_ids:
-            return []
-        payment_vals = []
-        currency_id = self.currency_id
-        for payment in self.payment_move_line_ids:
-            payment_currency_id = False
-            if self.type in ('out_invoice', 'in_refund'):
-                amount = sum([p.amount for p in payment.matched_debit_ids if p.debit_move_id in self.move_id.line_ids])
-                amount_currency = sum(
-                    [p.amount_currency for p in payment.matched_debit_ids if p.debit_move_id in self.move_id.line_ids])
-                if payment.matched_debit_ids:
-                    payment_currency_id = all([p.currency_id == payment.matched_debit_ids[0].currency_id for p in
-                                               payment.matched_debit_ids]) and payment.matched_debit_ids[
-                                              0].currency_id or False
-            elif self.type in ('in_invoice', 'out_refund'):
-                amount = sum(
-                    [p.amount for p in payment.matched_credit_ids if p.credit_move_id in self.move_id.line_ids])
-                amount_currency = sum([p.amount_currency for p in payment.matched_credit_ids if
-                                       p.credit_move_id in self.move_id.line_ids])
-                if payment.matched_credit_ids:
-                    payment_currency_id = all([p.currency_id == payment.matched_credit_ids[0].currency_id for p in
-                                               payment.matched_credit_ids]) and payment.matched_credit_ids[
-                                              0].currency_id or False
-            # get the payment value in invoice currency
-            if payment_currency_id and payment_currency_id == self.currency_id:
-                amount_to_show = amount_currency
-                amount_mxn = amount * self.currency_id.with_context(date=payment.date).rate
-            else:
-                currency = payment.company_id.currency_id
-                amount_to_show = currency._convert(amount, self.currency_id, payment.company_id, payment.date or fields.Date.today())
-                amount_mxn = currency._convert(amount, self.currency_id, payment.company_id, payment.date or fields.Date.today())
-            if float_is_zero(amount_to_show, precision_rounding=self.currency_id.rounding):
-                continue
-            payment_ref = payment.move_id.name
-            invoice_view_id = None
-            if payment.move_id.ref:
-                payment_ref += ' (' + payment.move_id.ref + ')'
-            if payment.invoice_id:
-                invoice_view_id = payment.invoice_id.get_formview_id()
-            payment_vals.append({
-                'name': payment.name,
-                'journal_name': payment.journal_id.name,
-                'amount': amount_to_show,
-                'amount_mxn': amount_mxn,
-                'currency': currency_id.symbol,
-                'digits': [69, currency_id.decimal_places],
-                'position': currency_id.position,
-                'date': payment.date,
-                'payment_id': payment.id,
-                'account_payment_id': payment.payment_id.id,
-                'invoice_id': payment.invoice_id.id,
-                'invoice_view_id': invoice_view_id,
-                'move_id': payment.move_id.id,
-                'ref': payment_ref,
-            })
-        return payment_vals
-
 class MailTemplate(models.Model):
     "Templates for sending email"
     _inherit = 'mail.template'
