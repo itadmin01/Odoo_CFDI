44ca03728537269d8618275da6819c7ce560193e
Fernando Rivero <frivero@itadmin.com.mx>
2022-07-25 08:54:43 -0500

    [FIX] change version

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '13.15',
+    'version': '13.16',
     'description': ''' Factura Electronica módulo de ventas para Mexico (CFDI 4.0)
     ''',
     'category': 'Accounting',
