a86ea825e201b416cd81fb3ec0698ea476b49a9f
Fernando Rivero <frivero@itadmin.com.mx>
2022-08-04 12:59:33 -0500

    [FIX] empty taxes

--- cdfi_invoice/models/account_invoice.py
+++ cdfi_invoice/models/account_invoice.py
@@ -511,9 +511,9 @@ class AccountMove(models.Model):
         tax_local_tras_tot = round(tax_local_tras_tot, no_decimales)
         tax_local_ret_tot = round(tax_local_ret_tot, no_decimales)
         self.discount = round(self.discount, no_decimales)
-        self.subtotal = self.roundTraditional(self.subtotal,no_decimales)
+        self.subtotal = self.roundTraditional(self.subtotal, no_decimales)
+        impuestos = {}
         if tax_grouped_tras or tax_grouped_ret:
-                impuestos = {}
                 retenciones = []
                 traslados = []
                 if tax_grouped_tras:
@@ -545,7 +545,7 @@ class AccountMove(models.Model):
                                          })
                    impuestos.update({'retenciones': retenciones, 'TotalImpuestosRetenidos': self.set_decimals(ret_tot, no_decimales)})
                 request_params.update({'impuestos': impuestos})
-                self.tax_payment = json.dumps(impuestos)
+        self.tax_payment = json.dumps(impuestos)
 
         if tax_local_ret or tax_local_tras:
            if tax_local_tras and not tax_local_ret:
