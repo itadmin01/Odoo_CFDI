a504e61c1ae0aa5209d02c3b317aeade15b43ba9
Fernando Rivero <frivero@itadmin.com.mx>
2022-08-27 09:43:28 -0500

    [FIX] update check vat

--- cdfi_invoice/models/res_partner.py
+++ cdfi_invoice/models/res_partner.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models, _
+from odoo import fields, models, _, api
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -10,26 +10,16 @@ class ResPartner(models.Model):
     uso_cfdi_id  =  fields.Many2one('catalogo.uso.cfdi', string='Uso CFDI (cliente)')
     regimen_fiscal_id  =  fields.Many2one('catalogo.regimen.fiscal', string='Régimen Fiscal')
 
-#    def check_vat(self):
-#        if self.env.context.get('company_id'):
-#            company = self.env['res.company'].browse(self.env.context['company_id'])
-#        else:
-#            company = self.env.company
-#        if company.vat_check_vies:
-            # force full VIES online check
-#            check_func = self.vies_vat_check
-#        else:
-            # quick and partial off-line checksum validation
-#            check_func = self.simple_vat_check
-#        for partner in self:
-#            if not partner.vat:
-#                continue
-            #check with country code as prefix of the TIN
-#            vat_country, vat_number = self._split_vat(partner.vat)
-#            if not check_func(vat_country, vat_number):
-                #if fails, check with country code from country
-#                country_code = 'MX' #partner.commercial_partner_id.country_id.code
-#                if country_code:
-#                    if not check_func(country_code.lower(), partner.vat):
-#                        msg = partner._construct_constraint_msg(country_code.lower())
-#                        raise ValidationError(msg)
+    @api.constrains('vat', 'country_id')
+    def check_vat(self):
+        # The context key 'no_vat_validation' allows you to store/set a VAT number without doing validations.
+        # This is for API pushes from external platforms where you have no control over VAT numbers.
+        if self.env.context.get('no_vat_validation'):
+            return
+
+        for partner in self:
+            country = self.env['res.country'].search([('code', '=', 'MX')])
+            if partner.vat and self._run_vat_test(partner.vat, country, partner.is_company) is False:
+                partner_label = _("partner [%s]", partner.name)
+                msg = partner._build_vat_error_message(country and country.code.lower() or None, partner.vat, partner_label)
+                raise ValidationError(msg)
