6e06ac6cf31b5ca60244471a2310e8afadaddcb2
Fernando Rivero <frivero@itadmin.com.mx>
2023-03-24 16:50:51 -0500

    [FIX] do not show total translated tax

--- cdfi_invoice/models/account_invoice.py
+++ cdfi_invoice/models/account_invoice.py
@@ -538,7 +538,7 @@ class AccountMove(models.Model):
                                       'base': self.roundTraditional(line['base'], no_decimales),
                                       'tax_id': line['tax_id'],
                                       })
-                impuestos.update({'translados': traslados, 'TotalImpuestosTrasladados': self.set_decimals(tras_tot, no_decimales)})
+                impuestos.update({'translados': traslados, 'TotalImpuestosTrasladados': self.set_decimals(tras_tot, no_decimales) if tras_tot > 0 else ''})
             if tax_grouped_ret:
                 for line in tax_grouped_ret.values():
                     tax = self.env['account.tax'].browse(line['tax_id'])
