c3c462d091d4ce3ee6c96e9ddab1cdb4f773cb40
Fernando Rivero <frivero@itadmin.com.mx>
2023-05-22 06:19:48 -0500

    [FIX] objetoimp

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '14.16',
+    'version': '14.17',
     'description': ''' Factura Electronica módulo de ventas para Mexico (CFDI 4.0)
     ''',
     'category': 'Accounting',
