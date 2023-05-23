5c43adae687a1ec46c962fe2a1d5a679fc70b60d
Fernando Rivero <frivero@itadmin.com.mx>
2023-05-23 16:14:32 -0500

    [FIX] labels in report

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '16.07',
+    'version': '16.08',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
