e500c08d129847313e95e4771794305ccded8e51
Fernando Rivero <frivero@itadmin.com.mx>
2023-09-11 16:12:55 -0500

    Update __manifest__.py and registro_cambios.txt

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '12.28',
+    'version': '12.29',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
