636971441a974fdf5ccb5ad5aa153a73651bfa48
Fernando Rivero <frivero@itadmin.com.mx>
2023-09-20 06:02:30 -0500

    [FIX] a cuenta de tercero

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '16.16',
+    'version': '16.17',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
