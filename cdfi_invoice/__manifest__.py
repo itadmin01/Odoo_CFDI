53689089ae79ae9c165764445fae5288101d47fa
Fernando Rivero <frivero@itadmin.com.mx>
2023-09-28 16:43:30 -0500

    [FIX] add cargar xml on payment

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '12.29',
+    'version': '12.30',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
