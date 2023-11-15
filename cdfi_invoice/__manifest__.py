465ff585d087e3f55220d54d26e4015f0b5a413f
Fernando Rivero <frivero@itadmin.com.mx>
2023-11-15 13:31:18 -0500

    [FIX] return round

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '14.27',
+    'version': '14.28',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
