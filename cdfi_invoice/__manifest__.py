b66ec56d7c34ef9f189d9d1c3adc1eaffbd88fdf
Fernando Rivero <frivero@itadmin.com.mx>
2024-01-21 07:41:16 -0500

    [FIX] Payment attachments

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '17.02',
+    'version': '17.03',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
