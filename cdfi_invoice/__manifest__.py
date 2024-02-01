7f14b77171b775af46d168ff85a5907f7f39a17d
Fernando Rivero <frivero@itadmin.com.mx>
2024-02-01 09:13:14 -0500

    [FIX] Change cfdi invoice state

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '16.19',
+    'version': '16.20',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
