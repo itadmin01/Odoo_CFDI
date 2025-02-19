ef080e5e6d171d09b655c052b75c445889fa62b1
Fernando Rivero <frivero@x1carbon>
2025-02-19 18:23:00 -0500

    Update __init__.py, __init__.pyc, and 43 more files...

--- catalogos_cfdi/models/clave_unidad.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from openerp import models, fields, api
-
-class ClaveUnidad(models.Model):
-    _name = 'catalogos.clave_unidad'
-    _rec_name = 'nombre'
-
-    c_claveunidad = fields.Char(string="Clave de Unidad", required=True)
-    nombre = fields.Char()
