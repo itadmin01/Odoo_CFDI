a4b7c57e4809a38b4acd3c255f0516ab9c482e28
Fernando Rivero <frivero@x1carbon>
2025-08-22 15:33:02 -0500

    [FIX] Update concepts in CCE

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '15.36',
+    'version': '15.37',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
