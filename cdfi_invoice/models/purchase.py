f986719f85a9f5b935a36ae0f04f2a9001110a75
Fernando Rivero <frivero@x1carbon>
2025-12-01 09:38:32 -0500

    [FIX] Import file in PO

--- cdfi_invoice/models/purchase.py
+++ cdfi_invoice/models/purchase.py
@@ -1,84 +1,114 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, api, models,_
+from odoo import fields, api, models, _
 import ast
-class PurchaseOrder(models.Model):
-    _inherit = 'purchase.order'
 
-    factura_cfdi = fields.Boolean('Factura CFDI')
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    factura_cfdi = fields.Boolean("Factura CFDI")
     tipo_comprobante = fields.Selection(
-        selection=[('I', 'Ingreso'), 
-                   ('E', 'Egreso'),
-                    ('T', 'Traslado'),],
-        string='Tipo de comprobante',
+        selection=[
+            ("I", "Ingreso"),
+            ("E", "Egreso"),
+            ("T", "Traslado"),
+        ],
+        string="Tipo de comprobante",
     )
-    forma_pago_id  =  fields.Many2one('catalogo.forma.pago', string='Forma de pago')
+    forma_pago_id = fields.Many2one("catalogo.forma.pago", string="Forma de pago")
     methodo_pago = fields.Selection(
-        selection=[('PUE', 'Pago en una sola exhibición'),
-                   ('PPD', 'Pago en parcialidades o diferido'),],
-        string='Método de pago', 
+        selection=[
+            ("PUE", "Pago en una sola exhibición"),
+            ("PPD", "Pago en parcialidades o diferido"),
+        ],
+        string="Método de pago",
     )
-    uso_cfdi_id  =  fields.Many2one('catalogo.uso.cfdi', string='Uso CFDI (cliente)')
+    uso_cfdi_id = fields.Many2one("catalogo.uso.cfdi", string="Uso CFDI (cliente)")
     estado_factura = fields.Selection(
-        selection=[('factura_no_generada', 'Factura no generada'), ('factura_correcta', 'Factura correcta'), 
-                   ('problemas_factura', 'Problemas con la factura'), ('factura_cancelada', 'Factura cancelada'), ],
-        string='Estado de factura',
-        default='factura_no_generada',
-        readonly=True
-    )	
-    numero_cetificado = fields.Char(string='Numero de certificado')
-#    cetificaso_sat = fields.Char(string=_('Certificado SAT'))
-    folio_fiscal = fields.Char(string='Folio Fiscal')
-    fecha_certificacion = fields.Datetime(string='Fecha y Hora Certificación')
-#    cadena_origenal = fields.Char(string=_('Cadena Original del Complemento digital de SAT'))
-    selo_digital_cdfi = fields.Char(string='Sello Digital del CDFI')
-    selo_sat = fields.Char(string='Sello del SAT')
-    moneda = fields.Char(string='Moneda')
-    tipocambio = fields.Char(string='Tipo de cambio')
-#    folio = fields.Char(string=_('Folio'))
-#    version = fields.Char(string=_('Version'))   
-#    invoice_datetime = fields.Char(string=_('11/12/17 12:34:12'))
-    tipo_relacion = fields.Selection(
-        selection=[('01', 'Nota de crédito de los documentos relacionados'), 
-                   ('02', 'Nota de débito de los documentos relacionados'), 
-                   ('03', 'Devolución de mercancía sobre facturas o traslados previos'),
-                   ('04', 'Sustitución de los CFDI previos'), 
-                   ('05', 'Traslados de mercancías facturados previamente'),
-                   ('06', 'Factura generada por los traslados previos'), 
-                   ('07', 'CFDI por aplicación de anticipo'),],
-        string='Tipo relación',
+        selection=[
+            ("factura_no_generada", "Factura no generada"),
+            ("factura_correcta", "Factura correcta"),
+            ("problemas_factura", "Problemas con la factura"),
+            ("factura_cancelada", "Factura cancelada"),
+        ],
+        string="Estado de factura",
+        default="factura_no_generada",
+        readonly=True,
     )
-    uuid_relacionado = fields.Char(string='CFDI Relacionado')
-    company_cfdi = fields.Boolean(related="company_id.company_cfdi",store=True)
+    numero_cetificado = fields.Char(string="Numero de certificado")
+    #    cetificaso_sat = fields.Char(string=_('Certificado SAT'))
+    folio_fiscal = fields.Char(string="Folio Fiscal")
+    fecha_certificacion = fields.Datetime(string="Fecha y Hora Certificación")
+    #    cadena_origenal = fields.Char(string=_('Cadena Original del Complemento digital de SAT'))
+    selo_digital_cdfi = fields.Char(string="Sello Digital del CDFI")
+    selo_sat = fields.Char(string="Sello del SAT")
+    moneda = fields.Char(string="Moneda")
+    tipocambio = fields.Char(string="Tipo de cambio")
+    #    folio = fields.Char(string=_('Folio'))
+    #    version = fields.Char(string=_('Version'))
+    #    invoice_datetime = fields.Char(string=_('11/12/17 12:34:12'))
+    tipo_relacion = fields.Selection(
+        selection=[
+            ("01", "Nota de crédito de los documentos relacionados"),
+            ("02", "Nota de débito de los documentos relacionados"),
+            ("03", "Devolución de mercancía sobre facturas o traslados previos"),
+            ("04", "Sustitución de los CFDI previos"),
+            ("05", "Traslados de mercancías facturados previamente"),
+            ("06", "Factura generada por los traslados previos"),
+            ("07", "CFDI por aplicación de anticipo"),
+        ],
+        string="Tipo relación",
+    )
+    uuid_relacionado = fields.Char(string="CFDI Relacionado")
+    company_cfdi = fields.Boolean(related="company_id.company_cfdi", store=True)
 
     def action_view_invoice(self, invoices=False):
-        res = super(PurchaseOrder,self).action_view_invoice(invoices=invoices)
-        if res:
-            context = {} 
-            if res.get('context')==None:
-                res['context']={}
-            if res['context']:
-                context=ast.literal_eval(res['context'])
-            order = self[0] 
-            context.update({
-                'default_factura_cfdi' : order.factura_cfdi,
-                'default_tipo_comprobante' : order.tipo_comprobante,
-                'default_forma_pago_id' : order.forma_pago_id.id,
-                'default_methodo_pago' : order.methodo_pago,
-                'default_uso_cfdi_id' : order.uso_cfdi_id.id,
-                'default_estado_factura' : order.estado_factura,
-                'default_numero_cetificado' : order.numero_cetificado,
-                'default_folio_fiscal' : order.folio_fiscal,
-                'default_fecha_certificacion' : order.fecha_certificacion,
-                'default_selo_digital_cdfi' : order.selo_digital_cdfi,
-                'default_selo_sat' : order.selo_sat,
-                'default_moneda' : order.moneda,
-                'default_tipocambio' : order.tipocambio,
-                'default_tipo_relacion' : order.tipo_relacion,
-                'default_uuid_relacionado' : order.uuid_relacionado 
-                })
-            res['context'] = context
+        res = super(PurchaseOrder, self).action_view_invoice(invoices=invoices)
+        if not res:
+            return res
+        raw_context = res.get("context")
+        if raw_context is None:
+            context = {}
+        elif isinstance(raw_context, dict):
+            context = raw_context.copy()
+        elif isinstance(raw_context, (str, bytes)):
+            try:
+                parsed = ast.literal_eval(raw_context)
+                context = parsed if isinstance(parsed, dict) else {}
+            except Exception:
+                try:
+                    import json
+
+                    parsed = json.loads(raw_context)
+                    context = parsed if isinstance(parsed, dict) else {}
+                except Exception:
+                    context = {}
+        else:
+            context = {}
+
+        order = self[0]
+        context.update(
+            {
+                "default_factura_cfdi": order.factura_cfdi,
+                "default_tipo_comprobante": order.tipo_comprobante,
+                "default_forma_pago_id": order.forma_pago_id.id,
+                "default_methodo_pago": order.methodo_pago,
+                "default_uso_cfdi_id": order.uso_cfdi_id.id,
+                "default_estado_factura": order.estado_factura,
+                "default_numero_cetificado": order.numero_cetificado,
+                "default_folio_fiscal": order.folio_fiscal,
+                "default_fecha_certificacion": order.fecha_certificacion,
+                "default_selo_digital_cdfi": order.selo_digital_cdfi,
+                "default_selo_sat": order.selo_sat,
+                "default_moneda": order.moneda,
+                "default_tipocambio": order.tipocambio,
+                "default_tipo_relacion": order.tipo_relacion,
+                "default_uuid_relacionado": order.uuid_relacionado,
+            }
+        )
+        res["context"] = context
         return res
 
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:            			
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
