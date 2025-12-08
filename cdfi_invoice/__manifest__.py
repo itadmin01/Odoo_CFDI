9d5c3cb7bcbea385eb4afd2d6af0e01e700a82e9
Fernando Rivero <frivero@x1carbon>
2025-12-08 09:49:19 -0500

    [FIX] import EDI

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '18.21',
+    'version': '18.22',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
