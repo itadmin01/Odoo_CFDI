d1b486aeda3f7131575607833b636e50cdd0b57c
Fernando Rivero <frivero@x1carbon>
2026-01-13 05:42:09 -0500

    [FIX] pagos states

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '17.33',
+    'version': '17.34',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
