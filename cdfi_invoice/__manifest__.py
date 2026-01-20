4c3ecbff3dedda34627b895e56aa03b8cf264bdb
Fernando Rivero <frivero@x1carbon>
2026-01-20 13:44:30 -0500

    [FIX] remove no_decimales

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '18.24',
+    'version': '18.25',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
