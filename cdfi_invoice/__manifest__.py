3d959e92c227758ab68d2a40f74a977e526dac18
Fernando Rivero <frivero@x1carbon>
2026-04-23 11:04:12 -0500

    Update __manifest__.py, account_invoice.py, and registro_cambios.txt

--- cdfi_invoice/__manifest__.py
+++ cdfi_invoice/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     'name': 'Ventas Factura Electronica Mexico CFDI',
-    'version': '19.1.12',
+    'version': '19.1.13',
     'description': ''' Factura Electronica módulo de ventas para Mexico
     ''',
     'category': 'Accounting',
